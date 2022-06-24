4b741ea2a8765afa58ac5fddab212f805e016043
ProwlingLynx <tamezjavier@gmail.com>
2022-06-24 01:23:38 -0500

    Adding a path to static.

--- server/index.js
+++ server/index.js
@@ -5,7 +5,16 @@ const server =http.createServer(app);
 const {Server} = require('socket.io');
 const io = new Server(server);
 
-const port = 3000;
+const port = 5000;
 
 //Todo get path to public build;
-app.use(express.static());
+app.use(express.static('socket-io-learning/public'));
+
+app.get('/', (req, res) => {
+  console.log('Some one is here lol');
+  res.status(200).send();
+};
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
