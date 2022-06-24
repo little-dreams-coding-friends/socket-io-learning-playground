67734e9285a46abcd337ab3c98eddfcb66520e10
ProwlingLynx <tamezjavier@gmail.com>
2022-06-24 02:16:14 -0500

    Adding chat and message state. Adding message and chat handlers. Adding logs for incoming and outgoing messages.

---
+++ socket-io-learning/src/App.jsx
@@ -0,0 +1,37 @@
+import './App.css';
+import {io} from 'socket.io-client';
+import React, {useState} from 'react';
+
+function App() {
+  const [message, setMessage] = useState('');
+  const [chat, setChat] = useState('');
+  const socket = io('http://localhost:3001');
+  socket.on('connect', () => {
+    console.log('We have a front end connection');
+  });
+  //TODO: handle send message through socket
+  const handleResponse = (response) => {
+    console.log(response);
+    setChat(response);
+  };
+  //TODO: handle receive message through socket
+  const sendMessage = () => {
+    socket.emit('chatMessage', message, handleResponse);
+  };
+  //TODO:handle message maker from user.
+  const handleMessageState = (e) => {
+    setMessage(e.target.value);
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        This is where the messages will go.
+      </header>
+      <input type="text" value={message} onChange={handleMessageState} />
+      <button onClick={sendMessage}/>
+      <div>{chat}</div>
+    </div>
+  );
+}
+
+export default App;
