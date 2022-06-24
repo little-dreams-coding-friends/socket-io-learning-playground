cec1ec31078a8774538e51080df04ad0a141b14b
ProwlingLynx <tamezjavier@gmail.com>
2022-06-24 01:40:21 -0500

    Added a path to static files in server index.js. Added server logic and verified instance works as intended.

--- server/index.js
+++ server/index.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const app = express();
-const http = require('http');
-const server =http.createServer(app);
-const {Server} = require('socket.io');
-const io = new Server(server);
+// const http = require('http');
+// const server = http.createServer(app);
+// const {Server} = require('socket.io');
+// const io = new Server(server);
 
-const port = 5000;
+const port = 3001;
 
-//Todo get path to public build;
-app.use(express.static('socket-io-learning/public'));
+app.use(express.static('socket-io-learning/build'));
 
 app.get('/', (req, res) => {
   console.log('Some one is here lol');
   res.status(200).send();
-};
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
